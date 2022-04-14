f7644203731bf2d4ff7ea1234cd34ed02a48910d
Henry C. Budris <hbudris1@umbc.edu>
2022-04-14 10:27:38 -0400

    Primitive state where logic gates can be created

--- digital_logic_simulator/logic_gates/logic_gates.py
+++ digital_logic_simulator/logic_gates/logic_gates.py
@@ -2,14 +2,18 @@ import collections
 from typing import Type
 
 Pin = collections.namedtuple("Pin", ('name', 'status'))
+GatePin = collections.namedtuple("GatePin", ('gate', 'pin'))
+PinConnection = collections.namedtuple("PinConnection", ('o_pin', 'i_pin'))
 
+
+# TODO: Convert this to a django model
 
 class PinCollectionException(Exception):
     def __init__(self, *args: object) -> None:
         super().__init__(*args)
 
 class PinCollection:
-    def __init__(self, pins : list[str] = []) -> None:
+    def __init__(self, pins : list = []) -> None:
         self._pins = dict(zip(pins, [0]*len(pins)))
 
     def __getitem__(self, name : str) -> collections.namedtuple:
@@ -31,6 +35,9 @@ class PinCollection:
             self._pins.pop(name)
         except KeyError:
             raise PinCollectionException(f"A pin with the name {name} cannot be removed as it does not exist")
+    
+    def __repr__(self):
+        return f"{self._pins}"
 
 '''
 Base class for all logic gates to inherit from.  This gate is a generic gate
@@ -40,7 +47,7 @@ Logic gates are only aware of their own inputs and produce the corresponding out
 An external manager will control signal and clock cycle management.
 '''
 class LogicGate:
-    def __init__(self, name : str, inputs : list[str] = [], outputs : list[str] = []) -> None:
+    def __init__(self, name : str, inputs : list = [], outputs : list = []) -> None:
         self._name = name
         self._inputs = PinCollection(inputs)
         self._outputs = PinCollection(outputs)
@@ -63,20 +70,23 @@ class LogicGate:
         self._name = name
 
     @property
-    def inputs(self) -> list[Pin]:
+    def inputs(self) -> list:
         return self._inputs
 
     @property
-    def outputs(self) -> list[Pin]:
+    def outputs(self) -> list:
         return self._outputs
 
-    def add_inputs(self, inputs : list[str]):
+    def add_inputs(self, inputs : list):
         for i in inputs:
             self._inputs += i
 
-    def add_outputs(self, outputs : list[str]):
+    def add_outputs(self, outputs : list):
         for o in outputs:
             self._outputs += o
+
+    def __repr__(self):
+        return f"Logic gate '{self.name}' \n\tInputs: {self._inputs} \n\tOutputs: {self._outputs}"
 
 class LogicGateManagerException(Exception):
     def __init__(self, *args: object) -> None:
@@ -103,11 +113,27 @@ class LogicGateManager:
     def __iadd__(self, gate : LogicGate) -> None:
         if type(gate) != LogicGate:
             raise TypeError(F"Only gates may be added to the logic gate manager, not {type(gate)}")
-        self._gates.append(gate)
+        self._gates.update({gate.name:{}})
 
     def __isub__(self, gate : LogicGate) -> None:
         if type(gate) != LogicGate:
-            raise TypeError(F"Only gates may be added to the logic gate manager, not {type(gate)}")
-        self._gates.remove(gate)
+            raise TypeError(F"Only gates may be removed to the logic gate manager, not {type(gate)}")
+        self._gates.pop(gate.name)
 
-    
+    def add_connection(self, output_gate : GatePin, input_gate : GatePin):
+        if not self._gates.haskey(input_gate.gate) or not self._gates.haskey(output_gate.gate):
+            raise LogicGateManagerException(f"Cannot create a conneection between {output_gate.gate} and {input_gate.gate} because at least one gate does not exist")
+
+        connections = self._gates.get(input_gate.gate).get(output_gate.gate)
+        self._gates[input_gate.gate].update({output_gate.gate: connections.append(PinConnection(output_gate.pin, input_gate.pin))})
+
+    def __repr__(self):
+        return ''
+
+def main():
+    gate1 = LogicGate("G1", ['A', 'B'], ['O'])
+    gate2 = LogicGate("G2", ['A', 'B'], ['O'])
+    print(gate1)
+    print(gate2)
+
+main()
