aa488865f93f52a0580bd539ef4df56b9ba743c7
Josh Shin <joshuas3@umbc.edu>
2022-04-14 10:23:35 -0400

    Adds in example files from example project, adds in image of logic gate to HTML

--- digital_logic_simulator/settings.py
+++ digital_logic_simulator/settings.py
@@ -21,17 +21,19 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = config('SECRET_KEY')
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = config('DEBUG', cast = bool)
+DEBUG = True
 
 ALLOWED_HOSTS = []
 
 
 # Application definition
 
+#'logicsim', Copy and paste than in 2 lines below
 INSTALLED_APPS = [
+    'logicsim',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
--- logicsim/models.py
+++ logicsim/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+
+#This would probably be changed later to suit our needs.
+class LogicGate(models.Model):
+    gate_type = models.TextField() #Type stored as textfield
+    image_url = models.TextField() #Image stored as textfield for now (need image links)
