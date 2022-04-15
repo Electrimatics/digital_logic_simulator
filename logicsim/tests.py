56c5099d30c2c35462038d354599b02603533016
Josh Shin <joshuas3@umbc.edu>
2022-04-14 22:45:28 -0400

    Adds in previous tests

--- logicsim/tests.py
+++ logicsim/tests.py
@@ -9,12 +9,21 @@ from django.urls import reverse
 # Create your views here.
 from logicsim.models import LogicGate
 
-class logicsimButtonTest(TestCase):
+class logicsimTest(TestCase):
     def test_add_element(self):
         logicsim = LogicGate.objects.filter()
         request = HttpRequest()
         request.method = 'GET'
         context = {
           'logicsim':logicsim,
+        }
+        return render(request, 'logicsim/displaytest.html', context=context)
+
+    def test_add_Button_element(self):
+        logicsim = LogicGate.objects.filter()
+        request = HttpRequest()
+        request.method = 'GET'
+        context = {
+           'logicsim':logicsim,
         }
         return render(request, 'logicsim/buttonTest.html', context=context)
