f7e5d651f936dff8737fdf97ffffb340c99d72c9
Josh Shin <joshuas3@umbc.edu>
2022-04-14 20:07:01 -0400

    Adds in unit tests

--- logicsim/tests.py
+++ logicsim/tests.py
@@ -1,3 +1,20 @@
-from django.test import TestCase
+from unittest import TestCase
+from urllib.request import Request
+from django.shortcuts import render
+from django.http import HttpResponse, HttpResponseRedirect, HttpRequest
+import urllib.request
 
-# Create your tests here.
+from django.urls import reverse
+
+# Create your views here.
+from logicsim.models import LogicGate
+
+class logicsimTest(TestCase):
+    def test_add_element(self):
+        logicsim = LogicGate.objects.filter()
+        request = HttpRequest()
+        request.method = 'GET'
+        context = {
+          'logicsim':logicsim,
+        }
+        response = render(request, 'logicsim/index.html', context=context)
