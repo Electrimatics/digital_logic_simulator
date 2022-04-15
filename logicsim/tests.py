929fc8002fa51b26d4344b8e6133378b2ab4f618
Josh Shin <joshuas3@umbc.edu>
2022-04-14 22:40:02 -0400

    Adds in tests and button file under templates

---
+++ logicsim/tests.py
@@ -0,0 +1,20 @@
+from unittest import TestCase
+from urllib.request import Request
+from django.shortcuts import render
+from django.http import HttpResponse, HttpResponseRedirect, HttpRequest
+import urllib.request
+
+from django.urls import reverse
+
+# Create your views here.
+from logicsim.models import LogicGate
+
+class logicsimButtonTest(TestCase):
+    def test_add_element(self):
+        logicsim = LogicGate.objects.filter()
+        request = HttpRequest()
+        request.method = 'GET'
+        context = {
+          'logicsim':logicsim,
+        }
+        return render(request, 'logicsim/buttonTest.html', context=context)
