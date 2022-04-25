6db125b75ffb3a1b53ac1f8e0efdc9eed653fd0b
Henry C. Budris <hbudris1@umbc.edu>
2022-04-25 17:44:52 -0400

    Added files necessary for Heroku integration

---
+++ Procfile
@@ -0,0 +1,2 @@
+release: python manage.py migrate
+web: gunicorn digital_logic_simulator.wsgi --log-file -
--- digital_logic_simulator/settings.py
+++ digital_logic_simulator/settings.py
@@ -118,6 +118,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
+import os
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_URL = '/static/'
+import django_heroku
+django_heroku.settings(locals())
+
 STATIC_URL = 'static/'
 
 # Default primary key field type
