e76b06f323b8750bf9c8990f6f7947832f411239
Tommy Nguyen <er.tommynguyen@gmail.com>
2022-04-26 20:04:29 -0400

    Added testing for update and delete

--- logicsim/tests.py
+++ logicsim/tests.py
@@ -28,3 +28,25 @@ class logicsimTest(TestCase):
         }
         return render(request, 'logicsim/buttonTest.html', context=context)
 
+    def test_update_element(self):
+      and_gate = LogicGate(gate_type="and", image_url=".jpg")
+      and_gate.save()
+
+      self.assertEqual(and_gate.gate_type, "and")
+      
+      and_gate.gate_type = "or"
+      and_gate.image_url = ".png"
+      and_gate.save()
+
+      self.assertEqual(and_gate.gate_type, "or")
+      self.assertEqual(and_gate.image_url, ".png")
+
+      def test_delete_element(self):
+        and_gate = LogicGate(gate_type="and", image_url=".jpg")
+        and_gate.save()
+
+        self.assertEqual(len(LogicGate.objects.all()), 1)
+        
+        and_gate.delete()
+
+        self.assertEqual(len(LogicGate.objects.all()), 0)
