8357ebe90646e3ba323fb2bf1b78a11b6ca291cc
Fahim-A-Karim <98853726+Fahim-A-Karim@users.noreply.github.com>
2022-04-27 21:35:04 -0400

    database

--- logicsim/models.py
+++ logicsim/models.py
@@ -4,5 +4,30 @@ from django.db import models
 
 #This would probably be changed later to suit our needs.
 class LogicGate(models.Model):
-    gate_type = models.TextField() #Type stored as textfield
-    image_url = models.TextField() #Image stored as textfield for now (need image links)
+    gate_id = models.AutoField(primary_key=True)
+
+    AND = 'and'
+    NAND = 'nand'
+    OR = 'or'
+    NOR = 'nor'
+    XOR = 'xor'
+    XNOR = 'xnor'
+    NOT = 'not'
+    gate_choices = (
+        (AND, "AND"),
+        (NAND, "NAND"),
+        (OR, "OR"),
+        (NOR, "NOR"),
+        (XOR, "XOR"),
+        (XNOR, "XNOR"),
+        (NOT, "NOT")
+    )
+    gate_type = models.CharField(max_length=9,
+                             choices=gate_choices,
+                             default="AND")
+
+    input_a = models.IntegerField()
+    input_b = models.IntegerField()
+    output = models.IntegerField()
+    #image_url = models.TextField() prob able to do based on gate_type
+
