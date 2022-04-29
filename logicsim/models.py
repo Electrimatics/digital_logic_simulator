8c483904dbe94567735438d40e8a09ed90fc40cc
Josh Shin <joshuas3@umbc.edu>
2022-04-28 22:22:47 -0400

    Reverts old commits and adds new ones

--- logicsim/models.py
+++ logicsim/models.py
@@ -4,6 +4,8 @@ from django.db import models
 
 #This would probably be changed later to suit our needs.
 class LogicGate(models.Model):
+    #you have an id col, then gate type(may need to be edited to account for custom components)
+    #then input output cols, these may need to edited based on how we configure connections and stuff
     gate_id = models.AutoField(primary_key=True)
 
     AND = 'and'
