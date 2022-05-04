035ebef42ad3c57c69cfa07af1ae49d44202e8f5
Tommy Nguyen <er.tommynguyen@gmail.com>
2022-05-04 16:25:21 -0400

    Fixed web server crash

--- logicsim/urls.py
+++ logicsim/urls.py
@@ -7,7 +7,7 @@ urlpatterns = [
     path('', views.index, name = 'index'),
     path('createGate/', views.createGate, name='createGate'),
     path('add/',views.add, name='add'),
-    path('update/<int:id>',views.update, name='update'),
-    path('delete/<int:id>',views.delete, name='delete')
+    path('update/<int:gate_id>', views.update, name='update'),
+    path('delete/<int:gate_id>', views.delete, name='delete')
 
 ]
